b457c9b7fc4e12fb4dc860f8b55d50c956a98a6a
Eid Aden <e-aden@hotmail.de>
2025-10-28 13:43:00 +0100

    Added hero section animations, scroll indicator component, and updated logo + social icons styling

--- src/app/landing/landing-page/landing-page.ts
+++ src/app/landing/landing-page/landing-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero/hero';
 import { Logo } from '../logo/logo';
 import { SocialIcon } from '../social-icon/social-icon';
+import { ScrollIndicator } from '../scroll-indicator/scroll-indicator';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Hero, Logo, SocialIcon],
+  imports: [Hero, Logo, SocialIcon, ScrollIndicator],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
---
+++ src/app/landing/scroll-indicator/scroll-indicator.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-indicator',
+  imports: [],
+  templateUrl: './scroll-indicator.html',
+  styleUrl: './scroll-indicator.scss',
+})
+export class ScrollIndicator {
+
+}
