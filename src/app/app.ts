365ca344aa60643f6801dfde2d1836349465c6a3
Eid Aden <e-aden@hotmail.de>
2025-11-07 17:50:15 +0100

    Navbar content added, and Footer & Why Me sections implemented

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPage } from './landing/landing-page/landing-page';
+import { Nav } from './shared/nav/nav';
+import { Footer } from './shared/footer/footer';
+import { WhyMe } from './why-me/why-me';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingPage],
+  imports: [RouterOutlet, LandingPage, Nav, Footer, WhyMe],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
