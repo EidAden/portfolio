2b3999d0472d3869957c3031a1fc72ebcf95833e
Eid Aden <e-aden@hotmail.de>
2025-11-22 14:53:57 +0100

    contan from is added

--- src/app/landing/my-projects/my-projects.ts
+++ src/app/landing/my-projects/my-projects.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-projects',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-projects.html',
   styleUrl: './my-projects.scss',
 })
 export class MyProjects {
-
+  selectedTab = 'join';
 }
